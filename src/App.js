19586e219c82b2ed0c2c38613d88522682758029
Guilherme Almeida <guilherme.dev.almeida@gmail.com>
2023-03-27 21:48:42 -0300

    ClassName Correction

--- src/App.js
+++ src/App.js
@@ -12,25 +12,25 @@ export default function App() {
 
 
 
-      <div class="content-wrapper">
-        <div class="text-wrapper">
-          <p class="review-text">“Estive interessada em código por um tempo mas não tomava uma atitude, até agora. Não
+      <div className="content-wrapper">
+        <div className="text-wrapper">
+          <p className="review-text">“Estive interessada em código por um tempo mas não tomava uma atitude, até agora. Não
             consigo recomendar esse curso o suficiente. Estou no trabalho dos meu sonhos e vejo um futuro
             brilhante!”</p>
-          <div class="author__content-wrapper">
-            <div class="author__name">Gabriela da Silva</div>
-            <div class="author__title">Desenvolvedora Fullstack</div>
+          <div className="author__content-wrapper">
+            <div className="author__name">Gabriela da Silva</div>
+            <div className="author__title">Desenvolvedora Fullstack</div>
           </div>
         </div>
 
-        <div class="carousel">
-          <div class="carousel__img-wrapper">
-            <img src={gabriela} alt="bootcamp graduate" class="carousel__img" />
+        <div className="carousel">
+          <div className="carousel__img-wrapper">
+            <img src={gabriela} alt="bootcamp graduate" className="carousel__img" />
           </div>
-          <div class="carousel__btn-wrapper">
-            <button id="btn-prev" class="btn btn--previous"><img src={icnPrev}
+          <div className="carousel__btn-wrapper">
+            <button id="btn-prev" className="btn btn--previous"><img src={icnPrev}
               alt="previous button" /></button>
-            <button id="btn-next" class="btn btn--next"><img src={icnNext}
+            <button id="btn-next" className="btn btn--next"><img src={icnNext}
               alt="next button" /></button>
           </div>
         </div>
